c9d5e993efb86bb18247084f0c860aa732854026
jaykumarM5 <jaikumar.saganjel@gmail.com>
2020-11-30 02:01:46 +0530

    updated about page

--- Application Code/Application/lib/Data.dart
+++ Application Code/Application/lib/Data.dart
@@ -5,6 +5,15 @@ import 'package:shared_preferences/shared_preferences.dart';
 String email = '';
 String otp = '1';
 
+String jayInsta = 'https://instagram.com/little.j.hobbit?igshid=14np6usrilpyp';
+String jayLinkedIn = 'https://linkedin.com/in/jay-kumar-smit';
+String surajInsta =
+    'https://instagram.com/surajkumarojha9123?igshid=eg6o81e2v0pf';
+String surajLinkedIn = 'https://www.linkedin.com/in/suraj-kumar-ojha-b51511194';
+
+String rishabhInsta = 'https://www.instagram.com/rishabh.live/';
+String rishabhLinkedIn = 'https://www.linkedin.com/in/rishabh0508/';
+
 String otpUrl =
     'https://i-voted.vercel.app/api/v1/setotp/rishabh12536@gmail.com';
 

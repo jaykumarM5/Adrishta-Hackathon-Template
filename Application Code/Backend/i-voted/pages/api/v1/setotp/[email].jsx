7d2962190f594e47099818b33596e54ef4644f66
rishabh-live <rishabh12536@gmail.com>
2020-11-29 22:09:07 +0530

    OTP page completed

---
+++ Application Code/Backend/i-voted/pages/api/v1/setotp/[email].jsx
@@ -0,0 +1,60 @@
+export default function SendOtp(req, res) {
+  var otpVal = Math.floor(100000 + Math.random() * 100000 + 1);
+  var email = req.query.email;
+
+  // Send Email
+  var mailaddress = email;
+  var subject =
+    "Tech Adrishta 2.5 i-Voted Login Otp ( Mail id : $_" +
+    (
+      Date.now().toString(36) + Math.random().toString(36).substr(2, 5)
+    ).toUpperCase() +
+    " )";
+  var message =
+    "This is your One Time Password Login for Smit Yukti App: " + otpVal;
+
+  var mailOptions = {
+    from: "Tech Adrishta 2.5",
+    to: mailaddress,
+    subject: subject,
+    text: message,
+  };
+
+  var theHtml = "<h1> Your Otp for I-Voted is : " + otpVal + "</h1>";
+  const fetch = require("node-fetch");
+
+  let url = "https://api.sendinblue.com/v3/smtp/email";
+
+  let options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "api-key": process.env.SENDINBLUE_KEY,
+    },
+    body:
+      '{"sender":{"name":"Tech Adrishta 2.5 (i-Voted)","email":"smit.yukti.official@gmail.com"},"to":[{"email":"' +
+      mailaddress +
+      '","name":"' +
+      mailaddress +
+      '"}],"replyTo":{"email":"smit.yukti.official@gmail.com","name":"Tech Adrishta 2.5"},"subject":"' +
+      subject +
+      '","htmlContent":"' +
+      theHtml +
+      '"}',
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) =>
+      res.json({
+        query: json,
+        otp: otpVal,
+      })
+    )
+    .catch((err) =>
+      res.json({
+        query: err,
+      })
+    );
+}

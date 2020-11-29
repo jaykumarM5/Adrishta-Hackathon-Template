482e7bc4dabf23983aa3807b42b07b2f4ab99591
rishabh-live <rishabh12536@gmail.com>
2020-11-29 20:05:16 +0530

    Modified Home page Info

--- Application Code/Backend/i-voted/pages/index.jsx
+++ Application Code/Backend/i-voted/pages/index.jsx
@@ -2,5 +2,14 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <>Site Launched</>;
+  return (
+    <>
+      Tech Adristha 2.5 (Hackathon) <br />{" "}
+      <h2>
+        <i>Team - 16</i>
+      </h2>
+      <br />
+      Topic - 4) I Voted!
+    </>
+  );
 }

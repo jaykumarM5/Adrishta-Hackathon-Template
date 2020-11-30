3c0fc5aabd1cfe8071264c06cadbb6e5d0025f3b
rishabh-live <rishabh12536@gmail.com>
2020-11-30 18:29:04 +0530

    IPFS and Blockchain for Vote recipt Completed

---
+++ Application Code/Backend/i-voted/pages/api/v1/blockchain/[voter]/[data].jsx
@@ -0,0 +1,81 @@
+const IpfsHttpClient = require("ipfs-http-client");
+const MongoClient = require("mongodb").MongoClient;
+var url = process.env.MONGO_SERVER;
+
+export default async function main(req, res) {
+  const { globSource } = IpfsHttpClient;
+  const ipfs = IpfsHttpClient({
+    timeout: 10000,
+    host: "ipfs.infura.io",
+    port: "5001",
+    protocol: "https",
+  });
+  var file;
+
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("iVoted");
+
+    // Check if exist
+    dbo
+      .collection("voters")
+      .find({ user: req.query.voter })
+      .toArray(async function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        var lb = "X";  
+        console.log(lb);
+        if (result.length == 0) {
+
+          var cntnt =
+            "{" +
+            req.query.voter +
+            ':"' +
+            req.query.data +
+            '", date : "' +
+            new Date() +
+            '", pb : "X"}';
+          file = await ipfs.add(cntnt);
+          console.log("Log : Uploaded to IPFS > " + cntnt);
+          var myobj = { user: req.query.voter, lb: file.path };
+          dbo.collection("voters").insertOne(myobj, function (err, res) {
+            if (err) throw err;
+            console.log("Log : 1 document inserted ");
+            
+          });
+          res.json({ vrid: file.path });
+        } else {
+          lb = result[0].lb;
+          var cntnt =
+            "{" +
+            req.query.voter +
+            ':"' +
+            req.query.data +
+            '", date : "' +
+            new Date() +
+            '", pb : "' +
+            lb +
+            '"}';
+          file = await ipfs.add(cntnt);
+          var myobj = { $set: { user: req.query.voter, lb: file.path } };
+          var myquery = { user: req.query.voter };
+          console.log("Log : available");
+          dbo
+            .collection("voters")
+            .updateOne(myquery, myobj, function (err, res) {
+              if (err) throw err;
+              console.log("1 document updated");
+              db.close();
+            });
+          res.json({ vrid: file.path });
+        }
+        db.close();
+      });
+  });
+}
+
+async function AddToIPFS(cntnt) {
+  var Qmhash = await ipfs.add(cntnt);
+
+  return Qmhash;
+}

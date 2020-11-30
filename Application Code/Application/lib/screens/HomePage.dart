9331c2d42a17196d13bbe1e3e79520c1d4ebc2be
Suraj Kumar Ojha <59886792+SurajKumarOjha9123@users.noreply.github.com>
2020-11-30 14:07:41 +0530

    Update HomePage.dart
    Home page mod

--- Application Code/Application/lib/screens/HomePage.dart
+++ Application Code/Application/lib/screens/HomePage.dart
@@ -6,13 +6,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var flag = true;
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
         body: Container(
             child: Center(
-          child: Text('Home'),
+          child: flag?Center(child: Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Container(height: size.height*0.40,
+              child: Card(elevation: 10,shadowColor: Colors.purple,child: 
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.stretch,children: [
+                  InkWell(onTap: (){},child: Card(elevation: 10,shadowColor: Colors.black,child: Container(height:size.height*0.10,width: size.width*0.90,),)),
+                  InkWell(onTap: (){},child: Card(elevation: 10,shadowColor: Colors.black,child: Container(height:size.height*0.10,width: size.width*0.90,),)),
+                  InkWell(onTap: (){},child: Card(elevation: 10,shadowColor: Colors.black,child: Container(height:size.height*0.10,width: size.width*0.90,),))
+                ],),
+              ),),
+            ),
+          ),):
+          Center(child: Text('No ongoing election'),),
         )),
       ),
     );

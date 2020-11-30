9e9d7b32c799e71d0f3678bb0a48363872147f43
rishabh-live <rishabh12536@gmail.com>
2020-12-01 02:14:52 +0530

    Added Ban Voters System

--- Application Code/Backend/i-voted/pages/api/v1/blockchain/[voter]/[data].jsx
+++ Application Code/Backend/i-voted/pages/api/v1/blockchain/[voter]/[data].jsx
@@ -15,62 +15,73 @@ export default async function main(req, res) {
   MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("iVoted");
+    //check if ban request
 
-    // Check if exist
-    dbo
-      .collection("voters")
-      .find({ user: req.query.voter })
-      .toArray(async function (err, result) {
+    if (req.query.data == "ban") {
+      dbo.collection("banVoter").createIndex({ voterid: 1 }, { unique: true });
+
+      var myobj = {
+        voterid: req.query.voter,
+      };
+      dbo.collection("banVoter").insertOne(myobj, function (err, res) {
         if (err) throw err;
-        console.log(result);
-        var lb = "X";  
-        console.log(lb);
-        if (result.length == 0) {
-
-          var cntnt =
-            "{" +
-            req.query.voter +
-            ':"' +
-            req.query.data +
-            '", date : "' +
-            new Date() +
-            '", pb : "X"}';
-          file = await ipfs.add(cntnt);
-          console.log("Log : Uploaded to IPFS > " + cntnt);
-          var myobj = { user: req.query.voter, lb: file.path };
-          dbo.collection("voters").insertOne(myobj, function (err, res) {
-            if (err) throw err;
-            console.log("Log : 1 document inserted ");
-            
-          });
-          res.json({ vrid: file.path });
-        } else {
-          lb = result[0].lb;
-          var cntnt =
-            "{" +
-            req.query.voter +
-            ':"' +
-            req.query.data +
-            '", date : "' +
-            new Date() +
-            '", pb : "' +
-            lb +
-            '"}';
-          file = await ipfs.add(cntnt);
-          var myobj = { $set: { user: req.query.voter, lb: file.path } };
-          var myquery = { user: req.query.voter };
-          console.log("Log : available");
-          dbo
-            .collection("voters")
-            .updateOne(myquery, myobj, function (err, res) {
-              if (err) throw err;
-              console.log("1 document updated");
-              db.close();
-            });
-          res.json({ vrid: file.path });
-        }
-        db.close();
+        console.log("Log : 1 document inserted ");
       });
+    } else if (req.query.data == "alright") {
+      // Check if exist
+      dbo
+        .collection("voters")
+        .find({ user: req.query.voter })
+        .toArray(async function (err, result) {
+          if (err) throw err;
+          console.log(result);
+          var lb = "X";
+          console.log(lb);
+          if (result.length == 0) {
+            var cntnt =
+              "{ name :" +
+              req.query.voter +
+              '", date : "' +
+              new Date() +
+              '", pb : "X", vr :"' +
+              req.query.data +
+              '" }';
+            file = await ipfs.add(cntnt);
+            console.log("Log : Uploaded to IPFS > " + cntnt);
+            var myobj = { user: req.query.voter, lb: file.path };
+            dbo.collection("voters").insertOne(myobj, function (err, res) {
+              if (err) throw err;
+              console.log("Log : 1 document inserted ");
+            });
+            res.json({ vrid: file.path });
+          } else {
+            lb = result[0].lb;
+            var cntnt =
+              "{ name :" +
+              req.query.voter +
+              '", date : "' +
+              new Date() +
+              '", pb : "' +
+              lb +
+              '", vr :"' +
+              req.query.data +
+              '" }';
+            file = await ipfs.add(cntnt);
+            var myobj = { $set: { user: req.query.voter, lb: file.path } };
+            var myquery = { user: req.query.voter };
+            console.log("Log : available");
+            dbo
+              .collection("voters")
+              .updateOne(myquery, myobj, function (err, res) {
+                if (err) throw err;
+                console.log("1 document updated");
+                db.close();
+              });
+            res.json({ vrid: file.path });
+          }
+          db.close();
+        });
+    }
   });
 }
 

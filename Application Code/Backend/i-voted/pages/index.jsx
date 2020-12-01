9f8067c5a016190caa6f41f6b05c158c5eee8f84
rishabh-live <rishabh12536@gmail.com>
2020-12-01 20:44:14 +0530

    Added readme.md on main page

--- Application Code/Backend/i-voted/pages/index.jsx
+++ Application Code/Backend/i-voted/pages/index.jsx
@@ -1,15 +1,36 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
+import Showdown from "showdown";
+import HEAD from "next/head";
 
-export default function Home() {
+function M2H(resp) {
+  var converter = new Showdown.Converter({ metadata: true });
+  converter.setFlavor("github");
+  var text = resp;
+  var html = converter.makeHtml(text);
+
+  return html;
+}
+
+export default function Arpp({ res }) {
+  var html = M2H(res);
+
   return (
     <>
-      Tech Adristha 2.5 (Hackathon) <br />{" "}
-      <h2>
-        <i>Team - 16</i>
-      </h2>
-      <br />
-      Topic - 4) I Voted!
+      <HEAD>
+        <link
+          rel="stylesheet"
+          href="https://raw.githubusercontent.com/markdowncss/splendor/master/css/splendor.css"
+        />
+      </HEAD>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </>
   );
 }
+
+Arpp.getInitialProps = async (ctx) => {
+  const res = await fetch(
+    "https://raw.githubusercontent.com/jaykumarM5/Adrishta-Hackathon-Template/master/README.md"
+  ).then((x) => x.text());
+  // const todos = await res.json();
+
+  return { res };
+};
